982e8d71a91c905187ab3adc9ec880e0c6c354ac
hcrisCopy <1992370882@qq.com>
2026-08-29 14:01:38 +0800

    autoresearch: Use full training-duration event dilation instead of a fractiona

--- universal_neuron_adapter/evaluate.py
+++ universal_neuron_adapter/evaluate.py
@@ -211,7 +211,7 @@ def main() -> None:
     context_diverse_weight = 8.0 * duration_factor
     context_normality_weight = duration_factor
     final_dilation_width = 1 + 2 * round(12.0 * duration_factor)
-    final_dilation_weight = 0.75 * duration_factor
+    final_dilation_weight = duration_factor
     effective_gaussian_sigma = args.gaussian_sigma + 0.5 * duration_factor
 
     output = Path(args.out_dir)

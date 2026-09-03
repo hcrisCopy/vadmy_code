4986e312646da4433f69075da4394e0a54fef57f
hcrisCopy <1992370882@qq.com>
2026-09-03 15:50:16 +0800

    Revert "autoresearch: Expand the globally sparse witness jury to cover multi-layer evi"
    This reverts commit a5f8f50f8a35e46a656bdc5d2e85384e5b8bf055.

--- run_instructions/run_witness_vad_f3_2_dsanet.sh
+++ run_instructions/run_witness_vad_f3_2_dsanet.sh
@@ -80,7 +80,7 @@ for dataset in $DATASETS; do
       --videos-per-class 0 \
       --num-workers 4 \
       --cache-training-data \
-      --active-neurons 128 \
+      --active-neurons 32 \
       --temporal-width 64 \
       --eta-normal 1.0 \
       --eta-anomaly 0.25 \
